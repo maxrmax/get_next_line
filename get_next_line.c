320c0e2cbf9eb79b5422edee577c32e4bd513777
mring <mring@student.42heilbronn.de>
2023-12-05 16:47:28 +0100

    lala

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,18 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:01:28 by mring             #+#    #+#             */
-/*   Updated: 2023/11/20 14:34:04 by mring            ###   ########.fr       */
+/*   Updated: 2023/12/05 16:35:18 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//	ssize_t	read(int fd, void buf[.count], size_t count)
 char	*get_next_line(int fd)
 {
-	
 }
 
 int	main(void)
 {
-	
+	int	fd;
+
+	fd = open("test.txt", O_RDONLY);
 }
