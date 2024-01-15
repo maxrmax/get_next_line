6a9e7e2cbab95911ad9bbc0e5a84ce9beb1049c9
mring <mring@student.42heilbronn.de>
2024-01-15 16:37:53 +0100

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,36 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:01:28 by mring             #+#    #+#             */
-/*   Updated: 2023/12/05 16:35:18 by mring            ###   ########.fr       */
+/*   Updated: 2024/01/15 16:15:13 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 //	ssize_t	read(int fd, void buf[.count], size_t count)
 char	*get_next_line(int fd)
 {
+	static char	*buffer[10 + 1];
+
+	if (fd < 0 | )
+		return (NULL);
 }
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
+	char	*line;
 
+	line = malloc(1 * sizeof(char));
 	fd = open("test.txt", O_RDONLY);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
 }
