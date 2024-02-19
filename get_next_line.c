19b3a4c25051152ee1b904f38b9595883f77fa83
mring <mring@student.42heilbronn.de>
2024-02-19 16:23:07 +0100

    wrapping my head around

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:51:38 by mring             #+#    #+#             */
-/*   Updated: 2024/02/19 10:58:18 by mring            ###   ########.fr       */
+/*   Updated: 2024/02/19 16:15:19 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,30 @@
 char	*get_next_line(int fd)
 {
 
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, "text.txt" ,0) < 0)
+		return (NULL);
+	
 }
 
 int	main(void)
 {
+	int 	fd;
+	char 	*line;
 
+	fd = open("test.txt", O_RDONLY);
+	line = "";
+	while (line != NULL)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+	}
+	fd = close(fd);
+	return(0);
 }
 
+/*
+open, read, loop read, allocate read content until \n,
+create new string with the read content, clear allocation, repeat.
+Until end of file. don't forget to \0 end of string. Buffer size + 1
+
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:51:44 by mring             #+#    #+#             */
-/*   Updated: 2024/02/19 10:58:16 by mring            ###   ########.fr       */
+/*   Updated: 2024/02/19 11:38:22 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 # include <fcntl.h>
 # include <stdlib.h>
