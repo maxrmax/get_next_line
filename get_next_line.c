c281d74cbe37384479b1de6c985da411797f36f0
mring <mring@student.42heilbronn.de>
2024-02-19 10:58:29 +0100

    init

--- get_next_line.c
+++ get_next_line.c
@@ -5,37 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/20 14:01:28 by mring             #+#    #+#             */
-/*   Updated: 2024/01/15 16:15:13 by mring            ###   ########.fr       */
+/*   Created: 2024/02/19 10:51:38 by mring             #+#    #+#             */
+/*   Updated: 2024/02/19 10:58:18 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "get_next_line.h"
 
 //	ssize_t	read(int fd, void buf[.count], size_t count)
 char	*get_next_line(int fd)
 {
-	static char	*buffer[10 + 1];
 
-	if (fd < 0 | )
-		return (NULL);
 }
 
 int	main(void)
 {
-	int		fd;
-	char	*line;
 
-	line = malloc(1 * sizeof(char));
-	fd = open("test.txt", O_RDONLY);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 14:06:15 by mring             #+#    #+#             */
-/*   Updated: 2024/01/15 16:04:49 by mring            ###   ########.fr       */
+/*   Created: 2024/02/19 10:51:44 by mring             #+#    #+#             */
+/*   Updated: 2024/02/19 10:58:16 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
